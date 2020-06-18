65d2ae64a517d0dce00e216afab2df51729bcc37
Misha Dyukarev <misha21022001@mail.ru>
2020-06-18 18:43:01 +0300

    Add project

---
+++ Assets/Scripts/Model/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	public sealed class Bullet : Ammunition
+	{
+		private void OnCollisionEnter(Collision collision)
+		{
+			// дописать доп урон
+			var tempObj = collision.gameObject.GetComponent<ISetDamage>();
+
+			if (tempObj != null)
+			{
+				tempObj.SetDamage(new InfoCollision(_curDamage, Rigidbody.velocity));
+			}
+
+            DestroyAmmunition();
+		}
+	}
+}
---
+++ Assets/Scripts/View/AimUiText.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Geekbrains
+{
+	public class AimUiText : MonoBehaviour
+	{
+		private Aim[] _aims;
+		private Text _text;
+		private Button _button;
+		private int _countPoint;
+		private void Awake()
+		{
+			_aims = FindObjectsOfType<Aim>();
+			_text = GetComponent<Text>();
+		}
+
+		private void OnEnable()
+		{
+			foreach (var aim in _aims)
+			{
+				aim.OnPointChange += UpdatePoint;
+			}
+			
+			_button.onClick.AddListener(Call);
+		}
+
+		private void Call()
+		{
+			Debug.Log("Example");
+		}
+
+		private void OnDisable()
+		{
+			foreach (var aim in _aims)
+			{
+				aim.OnPointChange -= UpdatePoint;
+			}
+			
+			_button.onClick.RemoveListener(Call);
+		}
+
+		private void UpdatePoint()
+		{
+			var pointTxt = "очков";
+			++_countPoint;
+			if (_countPoint >= 5) pointTxt = "очков";
+			else if (_countPoint == 1) pointTxt = "очко";
+			else if (_countPoint < 5) pointTxt = "очка";
+			_text.text = $"Вы заработали {_countPoint} {pointTxt}";
+		}
+	}
+}
