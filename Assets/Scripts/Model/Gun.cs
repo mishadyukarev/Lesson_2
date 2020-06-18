f9329993df9894d2fcdd462fa6ae78655b6aa485
Misha Dyukarev <misha21022001@mail.ru>
2020-06-18 18:52:28 +0300

    Revert "Revert "Add project""
    This reverts commit bcb13e9d22bf07f471b828685471d9c92cac2a97.

---
+++ Assets/Scripts/Model/Gun.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Geekbrains
+{
+	public sealed class Gun : Weapon
+	{
+		public override void Fire()
+		{
+			if (!_isReady) return;
+			if (Clip.CountAmmunition <= 0) return;
+			var temAmmunition = Instantiate(Ammunition, _barrel.position, _barrel.rotation);//todo Pool object
+			temAmmunition.AddForce(_barrel.forward * _force);
+			Clip.CountAmmunition--;
+			_isReady = false;
+			Invoke(nameof(ReadyShoot), _rechergeTime);
+			//_timer.Start(_rechergeTime);
+		}
+	}
+}
---
+++ Assets/Scripts/View/UiInterface.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	public class UiInterface
+	{
+		private FlashLightUiText _flashLightUiText;
+
+		public FlashLightUiText LightUiText => _flashLightUiText ? _flashLightUiText :
+			(_flashLightUiText = Object.FindObjectOfType<FlashLightUiText>());
+
+		private FlashLightUiBar _flashLightUiBar;
+
+		public FlashLightUiBar FlashLightUiBar
+		{
+			get
+			{
+				if (!_flashLightUiBar)
+					_flashLightUiBar = Object.FindObjectOfType<FlashLightUiBar>();
+				return _flashLightUiBar;
+			}
+		}
+
+		private WeaponUiText _weaponUiText;
+
+		public WeaponUiText WeaponUiText
+		{
+			get
+			{
+				if (!_weaponUiText)
+					_weaponUiText = Object.FindObjectOfType<WeaponUiText>();
+				return _weaponUiText;
+			}
+		}
+
+		private SelectionObjMessageUi _selectionObjMessageUi;
+
+		public SelectionObjMessageUi SelectionObjMessageUi
+		{
+			get
+			{
+				if (!_selectionObjMessageUi)
+					_selectionObjMessageUi = Object.FindObjectOfType<SelectionObjMessageUi>();
+				return _selectionObjMessageUi;
+			}
+		}
+	}
+}
